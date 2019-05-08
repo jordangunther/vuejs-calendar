f6a8c2c680016fa0c72dccc1509b483a0a76d9e1
Jordan Gunther <jordankgunther@gmail.com>
2019-05-08 13:19:45 -0600

    added a vuex store to hold the month and the year data

--- src/components/App.vue
+++ src/components/App.vue
@@ -21,13 +21,13 @@
 <script>
     import CalendarDay from './CalendarDay.vue';
     export default {
-        data() {
-            return {
-                month: 5,
-                year: 2019
-            };
-        },
         computed: {
+            month() {
+                return this.$store.state.currentMonth;
+            },
+            year() {
+                return this.$store.state.currentYear;
+            },
             days() {
 
                 //Generating all days in current month
