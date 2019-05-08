812c946055aedd17d88eaf6f9533920cbdbb3225
Jordan Gunther <jordankgunther@gmail.com>
2019-05-08 16:22:24 -0600

    added functionality to display events depending on which day they were assigned to

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,13 +1,26 @@
 <template>
-    <div :class="classObject" @click="captureClick">{{ day.format('D') }}</div>
+    <div :class="classObject" @click="captureClick">
+        {{ day.format('D') }}
+        <ul class="event-list">
+            <li v-for="event in events">{{ event.description }}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
         props: [ 'day' ],
         computed: {
+            events() {
+                let mockData = [
+                    { description: 'Random event 1', date: this.$moment('2019-05-08', 'YYYY-MM-DD') },
+                    { description: 'Random event 2', date: this.$moment('2019-05-07', 'YYYY-MM-DD') },
+                    { description: 'Random event 3', date: this.$moment('2019-06-08', 'YYYY-MM-DD') }
+                ];
+                return mockData.filter(event => event.date.isSame(this.day, 'day'));
+            },
             classObject() {
-                let today = this.day.isSame(this.$moment(), 'day')
+                let today = this.day.isSame(this.$moment(), 'day');
                 return {
                     day: true,
                     today,
