c242b8214489f0ee7e293493f1162179d1633149
Jordan Gunther <jordankgunther@gmail.com>
2019-05-08 13:34:59 -0600

    added logic to increment and decrement the month when buttons are clicked in CurrentMonth

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,10 @@ export default new Vuex.Store({
     state: {
         currentYear: 2019,
         currentMonth: 5
+    },
+    mutations: {
+        setCurrentMonth(state, payload) {
+            state.currentMonth = payload;
+        }
     }
 });
